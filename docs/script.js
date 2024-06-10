9f8f1d1da5b0bb2920fd9f22ec7207136549015b
Lichfolky <lichfolky@gmail.com>
2024-06-10 16:36:12 +0200

    anim

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,30 @@
 console.log("Ciao!");
-
+/*
 dateElement = document.querySelector("#date");
 
 today = new Date();
 
 dateElement.innerText = today.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+*/
+
+
+function toggleMenu() {
+    let menuMobile = document.querySelector("nav.mobile");
+    if (activeMenu == true) {
+        menuMobile.classList.add("hide");
+        activeMenu = false;
+    } else {
+        menuMobile.classList.remove("hide");
+        activeMenu = true;
+    }
+}
+
+let activeMenu = false;
+let hamburgerElement = document.querySelector(".hamburger");
+let menuMobile = document.querySelector("nav.mobile");
+
+
+hamburgerElement.addEventListener("click",
+    () => menuMobile.classList.toggle("hide"));
+
+
